d168fec321b33d17f718f1b88c8d6e2209056fc5
Rafy Akbar <rafyakbar02@gmail.com>
2021-07-02 17:44:05 +0700

    simplified student dictionary structure and changed function name

--- src/student.py
+++ src/student.py
@@ -1,44 +1,37 @@
 import data_access
-from collections import OrderedDict
 
 
-# Represents a dictionary of students that is sorted by name.
-class Students:
+# Add student to dictionary. Return True if the student is added and saved,
+# return False if the student already exists
+def add_student(name, age, grade):
+    students = data_access.get_dict()
+    if name not in students:
+        students[name] = {'age': age, 'grade': grade}
+        data_access.save_dict(students)
+        return True
+    else:
+        return False
 
-    # Create an ordered dictionary of students from a filled or empty .txt file
-    def __init__(self):
-        self.students = {}
 
-    # Add student to dictionary. Return True if the student is added and saved,
-    # return False if the student already exists
-    def add_student(self, name, age, grade):
-        self.students = data_access.open_file()
-        if name not in self.students:
-            self.students[name] = {'age': age, 'grade': grade}
-            data_access.save_file(self.students)
-            return True
-        else:
-            return False
+# Delete student from dictionary. Return True if the student is removed and saved,
+# return False if the student is not in the dictionary
+def delete_student(name):
+    students = data_access.get_dict()
+    if name not in students:
+        return False
+    else:
+        del students[name]
+        data_access.save_dict(students)
+        return True
 
-    # Delete student from dictionary. Return True if the student is removed and saved,
-    # return False if the student is not in the dictionary
-    def delete_student(self, name):
-        self.students = data_access.open_file()
-        if name not in self.students:
-            return False
-        else:
-            del self.students[name]
-            data_access.save_file(self.students)
-            return True
 
-    # String representation of Students class
-    def __str__(self):
-        str = ""
-        self.students = data_access.open_file()
-        if not self.students:
-            return "No students yet in the database"
-        for student in self.students:
-            str += f"Name: {student}, " \
-                   f"Age: {self.students[student]['age']}, " \
-                   f"Grade: {self.students[student]['grade']}\n"
-        return str
+# Print all students with their information
+def print_students(self):
+    self.students = data_access.get_dict()
+    if not self.students:
+        return "No students yet in the database"
+    for student in self.students:
+        str += f"Name: {student}, " \
+               f"Age: {self.students[student]['age']}, " \
+               f"Grade: {self.students[student]['grade']}\n"
+    return str
